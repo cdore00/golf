81623be8da39f6827e1361b1667f3ca141d1fc12
cdore00 <cdore00@yahoo.ca>
2017-09-04 19:45:45 -0400

    Add files via upload

--- misc/util.js
+++ misc/util.js
@@ -1,6 +1,6 @@
 <!--
 
-var HOSTserv = "https://nodejs-mongo-persistent-cd-serv.1d35.starter-us-east-1.openshiftapps.com/";
+var HOSTserv = "http://127.0.0.1:3000/";
 // "http://127.0.0.1:3000/";
 // "http://192.168.2.188:8080/";
 //"http://127.0.0.1:3000/";
@@ -164,6 +164,42 @@ function setOpacity(obj, opacity) {
   obj.style.opacity = opacity/100;
 }
 
+function posY(obj) {                       
+var p = posObj(obj);             
+//alert("X:" + p.x + " Y:" + p.y); 
+return p.y;
+}
+
+function posX(obj) {                       
+var p = posObj(obj);             
+//alert("X:" + p.x + " Y:" + p.y); 
+return p.x;
+}
+
+function posObj(htmlelement){
+
+var e = htmlelement; 
+var offset = {x:0,y:0}; 
+while (e) 
+{ 
+    offset.x += e.offsetLeft; 
+    offset.y += e.offsetTop; 
+    e = e.offsetParent; 
+} 
+return (offset);
+}
+
+function getOffset(el) {
+    var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) )
+    {
+        _x += el.offsetLeft - el.scrollLeft + el.clientLeft;
+        _y += el.offsetTop - el.scrollTop + el.clientTop;
+        el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
+}
 
 function adjustScreen(hauteurUtil){
 	var divMap, dispH, pxRatio
