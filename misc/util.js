057bab8ab79cb8519b8b1e0728e140eb7e123809
cdore00 <cdore00@yahoo.ca>
2017-09-16 21:18:37 -0400

    Update util.js

--- misc/util.js
+++ misc/util.js
@@ -1,9 +1,9 @@
 <!--
 
-var HOSTserv = "https://nodejs-mongo-persistent-cd-serv.1d35.starter-us-east-1.openshiftapps.com/";
+var HOSTserv = "http://cdore.ddns.net:8080/";
 // "http://127.0.0.1:3000/";
 // "http://192.168.2.188:8080/";
-//"http://127.0.0.1:3000/";
+//"http://cdore.ddns.net:8080/";
 //"https://nodejs-mongo-persistent-cd-serv.1d35.starter-us-east-1.openshiftapps.com/";
 
 function is_touch_device() {
