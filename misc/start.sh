fc84f008b9bab4552cfa733825f651055058acd0
cdore00 <cdore00@yahoo.ca>
2017-10-04 18:52:57 -0400

    Update start.sh

--- misc/start.sh
+++ misc/start.sh
@@ -1,11 +1,11 @@
 # /etc/systemd/system/startapp.service    CALL
 # /etc/rc.local    CALL
 # location: /init
+sleep 40s
 FILE=start.txt
 FILE=${FILE%.*}_`date +%I:%M:%S`.${FILE#*.}
 #sudo wall $FILE
 sudo touch /home/$FILE
-sleep 40s
 sudo ip addr add 192.168.10.11/24 dev docker0
 sleep 5
 sudo docker start mon_golf
