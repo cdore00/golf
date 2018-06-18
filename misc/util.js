f3304e9e8b0861f76827afdb0ac1f783079af260
cdore <cdore00@yahoo.ca>
2018-06-17 23:14:25 -0400

    gif size

--- misc/util.js
+++ misc/util.js
@@ -153,7 +153,7 @@ if (sizeAD && typeof sizeAD == "number"){
 	if (pageZone)
 		pageZone.style.visibility = "visible";
 }
-resizeImg(eval(fs.replace("em", "")));
+resizeImg(fs);
 }
 
 function resizeImg(fs){
