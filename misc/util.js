dd4d28810db90db55c98caab83cf2af3c5bfd215
cdore <cdore00@yahoo.ca>
2018-07-13 22:22:39 -0400

    edit golf9

--- misc/util.js
+++ misc/util.js
@@ -1,5 +1,5 @@
 
-var HOSTserv = "https://pytgolf-cd-serv.1d35.starter-us-east-1.openshiftapps.com/";  // Python 3.6.3 
+var HOSTserv = "http://127.0.0.1:3000/";		//Portable Windows 10 Local host Node JS v6.10.0
 // "http://127.0.0.1:3000/";		//Portable Windows 10 Local host Node JS v6.10.0
 // "http://192.168.2.195:3000/";    //Ubuntu workstation 16.04
 // "http://192.168.2.195:8080/";    //Ubuntu workstation 16.04 docker 1.12.6 Node JS v4.2.3  MongoDB server v3.4.9
@@ -320,7 +320,8 @@ if (rep.resp.result){
 
 }else{
 	userId = null;
-	setIdent();
+	if (typeof setIdent != "undefined")
+		setIdent();
 	DelCookie("userID");
 	alert(langLbl["M0004"]);
 	tryLog++;
@@ -330,6 +331,7 @@ if (rep.resp.result){
 		}
 	}
 }
+if (typeof identified != "undefined")
 	identified();
 }
 
