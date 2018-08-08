6cda6a44702261cc17e382249723256064a4c8ad
cdore <cdore00@yahoo.ca>
2018-08-07 21:29:42 -0400

    score editclubMap

--- misc/util.js
+++ misc/util.js
@@ -1,10 +1,10 @@
 
-var HOSTserv = "https://nodegolf-cd-serv.1d35.starter-us-east-1.openshiftapps.com/";  // Openshift mon_golf34
+var HOSTserv = "https://pytgolf-cd-serv.1d35.starter-us-east-1.openshiftapps.com/";  // Python 3.6.3 
 // "http://127.0.0.1:3000/";		//Portable Windows 10 Local host Node JS v6.10.0
 // "http://192.168.2.195:3000/";    //Ubuntu workstation 16.04
 // "http://192.168.2.195:8080/";    //Ubuntu workstation 16.04 docker 1.12.6 Node JS v4.2.3  MongoDB server v3.4.9
 // "http://192.168.2.190:8080/";    //Fedora workstation 26 Mongo 3.2.16 docker 1.13.1 Node JS v4.2.3 MongoDB server v3.4.6
-// "https://nodegolf-cd-serv.1d35.starter-us-east-1.openshiftapps.com/";  // Openshift mon_golf34
+// "https://nodegolf-cd-serv.1d35.starter-us-east-1.openshiftapps.com/";  // Openshift mon_golf34  GET GET
 // "https://nodejs-mongo-persistent-cd-serv.1d35.starter-us-east-1.openshiftapps.com/";  // Openshift default docker Node Js -v 6.11.3
 // "https://cdore.ddns.net/node/";  // VULTR Ubuntu Server 16.04 docker Node Js -v 6.11.3
 // "https://cdore.ddns.net/pyt/";  // VULTR Ubuntu Server 16.04 docker Python 3.6.4
@@ -35,7 +35,7 @@ var xhr=new XMLHttpRequest();
 		callback(data);
 	};
 xhr.open(THCall, HOSTserv + path ,true);
-if (HOSTserv != "http://127.0.0.1:3000/")
+if (THCall == "POST" && HOSTserv != "http://127.0.0.1:3000/")
 	xhr.withCredentials = true;
 xhr.send(dat);
 
