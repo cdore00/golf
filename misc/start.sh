72eed8a304712ae56f250c22ceb0c09814c2361b
unknown <cdore00@yahoo.ca>
2024-02-01 21:53:02 -0500

    start.sh

--- misc/start.sh
+++ misc/start.sh
@@ -3,13 +3,13 @@
 # location: /etc/init
 sleep 40s
 FILE=start.txt
-FILE=${FILE%.*}_`date +%I:%M:%S`.${FILE#*.}
+FILE=${FILE%.*}_`date +%y-%m-%d:%M:%S`.${FILE#*.}
 #sudo wall $FILE
 sudo touch /home/$FILE
 sudo ip addr add 192.168.10.11/24 dev docker0
 sleep 5
 sudo docker start monDB
-sleep 20
+sleep 10
 sudo docker start servDB
 sudo docker start webServ
 
